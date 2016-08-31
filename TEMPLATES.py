8f926b04c875371f558034befbe36c5b31cb53c4
Letty <lsmartin@ucsd.edu>
2016-08-31 09:45:40 -0700

    added more phrases

--- TEMPLATES.py
+++ TEMPLATES.py
@@ -8,7 +8,8 @@ brandTemplate = ['%s is one of the better brands for these.',
     '%s is actually not that bad. ',
     'I personally am partial towards %s\'s stuff. ',
     'I like the way %s makes these. ',
-    '%s makes some pretty durable products. ']
+    '%s makes some pretty durable products. ',
+    '%s has been a reliable, go to, brand for me. ']
 
 categoryTemplate = ['This is probably the best %s. ',
     'This is my favorite when it comes to %s. ',
@@ -19,7 +20,8 @@ featuresTemplate = ['%s']
 
 priceTemplate = ['For less than %s, you can\'t go wrong. ',
     'It\'s a steal for less than %s. ',
-    'Even if %s seems a bit pricy, it\'s well worth the cost. ']
+    'Even if %s seems a bit pricy, it\'s well worth the cost. ',
+    'At %s, the price is on par with its quality.']
 
 reviewsTemplate = ['%s']
 
@@ -27,7 +29,24 @@ linkTemplate = ['I prefer %s. ',
     'I opted for %s and never looked back. ',
     'I\'ve always loved my %s. ',
     '%s hands down. ',
-    'You can do a lot worse than %s. ']
+    'You can do a lot worse than %s. ',
+    '%s is a possible choice.',
+    'I haven\'t been disappointed by %s.',
+    'Of the ones I\'ve tried, %s stands out.',
+    'I like %s.',
+    'Big fan of this one: %s.',
+    'Not sure if this is what you\'re looking for, but %s worked great for me.',
+    'I\'m suprised no one has mentioned %s.',
+    'Although I am no a huge enthusiast of the brand, %s stands out.',
+    'Ooooh, I like this personally: %s.']
 
 oLinkTemplate = ['While %s is great, there are some better options out there. ',
-    'I\'m not a huge fan of %s. ']
+    'I\'m not a huge fan of %s. ',
+    'While %s is a decent choice, there are other options to explore.',
+    'I have had a bad experience with %s, I would recommend something different.',
+    'Not the best quality from %s.',
+    'I find %s to be inconsistent personally.',
+    '%s isn\'t bad, but there are better options out there.',
+    'I started with %s myself, but have since then found some better products.',
+    'Not the best results from %s.',
+    '%s is solid, but not the best.']
