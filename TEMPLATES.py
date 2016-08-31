8a69eec30af3d9b1a9125c7b1fa3d6f878183234
Winson <winson.luk@gmail.com>
2016-08-31 08:56:02 -0700

    add templates

---
+++ TEMPLATES.py
@@ -0,0 +1,33 @@
+brandTemplate = ['%s is one of the better brands for these.',
+    '%s actully makes some quality stuff. ',
+    'You can\'t go wrong with %s. ',
+    'I\'ve been using %s for a while, and its products have never failed me. ',
+    '%s is pretty decent for stuff like this. ',
+    'Have you heard of %s? ',
+    'I\'ve been a customer of %s for quite some time. ',
+    '%s is actually not that bad. ',
+    'I personally am partial towards %s\'s stuff. ',
+    'I like the way %s makes these. ',
+    '%s makes some pretty durable products. ']
+
+categoryTemplate = ['This is probably the best %s. ',
+    'This is my favorite when it comes to %s. ',
+    'I\'ve wanted a nice %s for quite some time. ',
+    'I\'ve tried many brands for %s. ']
+
+featuresTemplate = ['%s']
+
+priceTemplate = ['For less than %s, you can\'t go wrong. ',
+    'It\'s a steal for less than %s. ',
+    'Even if %s seems a bit pricy, it\'s well worth the cost. ']
+
+reviewsTemplate = ['%s']
+
+linkTemplate = ['I prefer %s. ',
+    'I opted for %s and never looked back. ',
+    'I\'ve always loved my %s. ',
+    '%s hands down. ',
+    'You can do a lot worse than %s. ']
+
+oLinkTemplate = ['While %s is great, there are some better options out there. ',
+    'I\'m not a huge fan of %s. ']
